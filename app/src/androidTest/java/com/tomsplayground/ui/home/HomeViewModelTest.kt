bfb9fd71f4720c4584ef1543a1b922f9c29fa0bc
Tom Rowbotham <tom.rowbotham@gousto.co.uk>
2022-12-01 17:03:46 +0000

    Started work on testing

--- app/src/androidTest/java/com/tomsplayground/ui/home/HomeViewModelTest.kt
+++ app/src/androidTest/java/com/tomsplayground/ui/home/HomeViewModelTest.kt
@@ -1,19 +1,23 @@
 package com.tomsplayground.ui.home
 
+import com.google.common.truth.Truth
 import com.tomsplayground.domain.PostRepository
 import com.tomsplayground.domain.mapper.PostUiStateMapper
-import io.mockk.MockKAnnotations
+import com.tomsplayground.domain.model.Post
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
-import io.mockk.unmockkAll
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 
-class HomeViewModelTest{
+class HomeViewModelTest {
 
-    lateinit var homeViewModel: HomeViewModel
+    private lateinit var homeViewModel: HomeViewModel
 
     private val postUiStateMapper = PostUiStateMapper()
 
@@ -23,17 +27,69 @@ class HomeViewModelTest{
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-        homeViewModel = HomeViewModel(postRepository, postUiStateMapper)
     }
 
-    @Test
-    fun GIVEN_postsFromRepository_WHEN_getContent_THEN_shouldMapAsPostsUiState(){
+    //TODO: Get Init test to pass
+//    @Test
+//    fun GIVEN_postsFromRepository_WHEN_getContent_THEN_shouldMapAsPostsUiState() = runTest{
+//            //GIVEN
+//            val posts = getMockPosts()
+//            val expectedPosts = getMappedPostsUiState()
+//
+//            every { postRepository.getUserFeedPosts() } returns posts
+//
+//            //WHEN
+//            generateViewModel()
+//
+//            //THEN
+//            Truth.assertThat(homeViewModel.uiState.value.posts).isEqualTo(expectedPosts)
+//
+//        }
 
-    }
 
 
     @After
     fun tearDown() {
         unmockkAll()
+    }
+
+    private fun generateViewModel() {
+        homeViewModel = HomeViewModel(postRepository, postUiStateMapper)
+    }
+
+    private fun getMockPosts(): List<Post> {
+        val post1 = Post(
+            POST_AUTHOR,
+            AUTHOR_PFP,
+            "1",
+            IMAGE_URL,
+            CONTENT_DESC,
+            CAPTION
+        )
+        val post2 = Post(
+            POST_AUTHOR,
+            SECOND_POST_AUTHOR,
+            "2",
+            IMAGE_URL,
+            CONTENT_DESC,
+            CAPTION
+        )
+        return listOf(post1, post2)
+    }
+
+    private fun getMappedPostsUiState(): List<PostUiModel> {
+        return listOf(
+            PostUiModel(POST_AUTHOR, AUTHOR_PFP, IMAGE_URL, CONTENT_DESC, CAPTION),
+            PostUiModel(SECOND_POST_AUTHOR, AUTHOR_PFP, IMAGE_URL, CONTENT_DESC, CAPTION)
+        )
+    }
+
+    companion object {
+        const val POST_AUTHOR = "post_author"
+        const val AUTHOR_PFP = "author_pfp"
+        const val IMAGE_URL = "image_url"
+        const val CONTENT_DESC = "content_desc"
+        const val CAPTION = "caption"
+        const val SECOND_POST_AUTHOR = "second_post_author"
     }
 }
